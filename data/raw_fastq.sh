797951261dce2a18a2cfadea96c3611178082cac
IvlavIv <165831951+IvlavIv@users.noreply.github.com>
2025-06-24 11:23:03 +0500

    Create raw_fastq.sh

---
+++ data/raw_fastq.sh
@@ -0,0 +1,12 @@
+#1552449 
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR155/002/SRR1552449/SRR1552449.fastq.gz
+#1552451
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR155/002/SRR1552451/SRR1552451.fastq.gz
+#1552452
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR155/002/SRR1552452/SRR1552452.fastq.gz
+#1552453
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR155/002/SRR1552453/SRR1552453.fastq.gz
+#1552454
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR155/002/SRR1552454/SRR1552454.fastq.gz
+#1552455
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR155/002/SRR1552455/SRR1552455.fastq.gz
